2cd80967c15a8bd6a07e7e05a26b0a08b791b383
kevinjshah2207 <kevinjshah2207@gmail.com>
2021-11-25 14:51:45 -0700

    FPR and FNR completed

--- accuracy_metrics.py
+++ accuracy_metrics.py
@@ -1,11 +1,29 @@
 #  False Positive Rate = FP / (FP + TN)
-#  Miss Rate = No. of misses / Total no of requests
+#  Miss Rate = FNR = False negatives / (False negatives + True positives) 
 total = {1: 12, 2: 40, 3: 10}
 
-def fp_rate(real, obs, task):
+def metrics(real, obs, task):
+    '''
+    Input: Real Label Data Matrix, Predicted Label Data Matrix, Task Number
+
+    Output: False Positive Rate Matrix, False Negative Rate Matrix
+    '''
     tot = total[task]
-    result = []
-    for i in range(1, tot):
-        fp = 0
-        tn = 0
-        for i in range
+    fpr, fnr = [], []
+    for val in range(1, tot):
+        fp, tp, fn, tn = 0, 0, 0, 0
+        for j in range(len(real)):
+            if val != real and val != obs:
+                tn += 1
+            elif val == obs and val != real:
+                fp += 1
+            elif val == obs and val == real:
+                tp += 1
+            else:
+                fn += 1
+                continue
+        fpr_1 = fp/(fp+tn)
+        fnr_1 = fn/(fn+tp)
+        fpr.append(fpr_1)
+        fnr.append(fnr_1)
+    return fpr, fnr
