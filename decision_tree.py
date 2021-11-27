8852b3f7a48622691fef7f2e39677b2acfaeedf5
Aniket Devle <aniketdevle@gmail.com>
2021-11-26 21:57:38 -0700

    added decision tree and task6

--- decision_tree.py
+++ decision_tree.py
@@ -1,6 +1,137 @@
-from sklearn import tree
+import numpy as np
+import pickle
+import os
+import utilities
+import generate_data_matrix
+import svm
+import numpy as np
+import cv2
+# import aggregation
+import csv
+import min_max_scaler
+import decision_tree
+from sklearn.model_selection import train_test_split
 
-def fit(Data_matrix, label_matrix):
-    clf = tree.DecisionTreeClassifier()
-    clf.fit(Data_matrix,label_matrix)
-    return clf
+
+def class_counts(rows):
+    counts = {}
+    for row in rows:
+        label = row[-1]
+        if label not in counts:
+            counts[label] = 0
+        counts[label] += 1
+    return counts
+
+
+class Leaf_Node:
+
+    def __init__(self, rows):
+        self.future_pred = list(class_counts(rows).keys())[0]
+
+class D_Node:
+    def __init__(self,
+                 rule,
+                 left_br,
+                 right_br):
+        self.rule = rule
+        self.left_br = left_br
+        self.right_br = right_br
+
+
+class rule:
+    def __init__(self, attr, value):
+        self.attr = attr
+        self.value = value
+
+    def match(self, row):
+        val = row[self.attr]
+        if isinstance(self.value, int) or isinstance(self.value, float):
+            return val >= self.value
+        else:
+            return val == self.value
+
+
+class DecisionTreeClassifier():
+
+    def __init__(self):
+        self.root = None
+
+
+    def split(self, rows):
+        current_gain, uncertainty, counts, good_ques, best_gain = 0, 1, class_counts(rows), None, 0
+        for count in counts:
+            prob = counts[count] / float(len(rows))
+            uncertainty -= prob ** 2
+
+        for col in range(len(rows[0]) - 1):
+            values = set([row[col] for row in rows])
+            for val in values:
+                ques = rule(col, val)
+
+                left_rows, right_rows = [], []
+                for row in rows:
+                    if ques.match(row):
+                        left_rows.append(row)
+                    else:
+                        right_rows.append(row)
+
+                if len(left_rows) == 0 or len(right_rows) == 0:
+                    continue
+
+                gl_p = float(len(left_rows)) / (len(left_rows) + len(right_rows))
+                ginileft, giniright = 1, 1
+                countsleft, countsright = class_counts(left_rows), class_counts(right_rows)
+                for count in countsleft:
+                    prob = countsleft[count] / float(len(left_rows))
+                    ginileft -= prob ** 2
+                for count in countsright:
+                    prob1 = countsright[count] / float(len(right_rows))
+                    giniright -= prob1 ** 2
+                gain = uncertainty - gl_p * ginileft - (1 - gl_p) * giniright
+
+                if gain >= best_gain:
+                    best_gain, good_ques = gain, ques
+
+        return best_gain, good_ques
+
+
+    def make_tree(self, rows):
+        gain, rule = self.split(rows)
+        if gain == 0:
+            return Leaf_Node(rows)
+
+
+        left_rows, right_rows = [], []
+
+
+        for row in rows:
+            if rule.match(row):
+                left_rows.append(row)
+            else:
+                right_rows.append(row)
+
+        left_br, right_br = self.make_tree(left_rows), self.make_tree(right_rows)
+        return D_Node(rule, left_br, right_br)
+
+
+    def predict(self, row, node):
+        if node == None:
+            node = self.root
+        if isinstance(node, Leaf_Node):
+            return node.future_pred
+
+        if node.rule.match(row):
+            return self.predict(row, node.left_br)
+        else:
+            return self.predict(row, node.right_br)
+
+    def fit(self, data):
+        self.root = self.make_tree(data)
+        return self.root
+
+    def transform(self, data):
+        results = []
+        for row in data:
+            results.append(self.predict(row, self.root))
+        return results
+
---
+++ task6.py
@@ -0,0 +1,86 @@
+from decision_tree import DecisionTreeClassifier
+import numpy as np
+import os
+from task4 import get_top_images
+import cv2
+import matplotlib.pyplot as plt
+import utilities
+
+def get_feedback():
+    label_input = [int(i) for i in input.split("enter 1 or 0")]
+    return label_input
+
+def get_predictor_object(top_k_relevant, labels_for_top_k):
+
+    X_train = np.ndarray.tolist(top_k_relevant)
+
+    #before this you have to change the label from 1,0 to +1 and -1
+    y_train = np.ndarray.tolist(labels_for_top_k)
+    training_data = []
+    for i in range(len(X_train)):
+        a =[] 
+        a.extend(X_train[i])
+        a.append(y_train[i])
+        training_data.append(a)
+
+    clf = DecisionTreeClassifier()
+    node = clf.fit(training_data)
+    
+    return clf,node
+
+    
+def start_task6():
+    l = int(input('enter num of layers: '))
+    k = int(input('enter num of hashes per layer: '))
+    vector_file = input('enter vector file: ')
+    t = int(input('enter number of retrievals: '))
+    image_folder = os.path.join(os.getcwd(), input('enter image folder: '))
+    query_image_path = os.path.join(os.getcwd(), input('enter query image name') + '.png')
+    query_image = cv2.imread(query_image_path, cv2.IMREAD_GRAYSCALE)
+    
+    similarity_image_map, vector_file_tokens = get_top_images(
+        l, k, vector_file, t, image_folder, query_image
+    )
+
+    top_images = similarity_image_map[:t]
+    fig, axes = plt.subplots(t + 1, 1)
+    for i, axis in enumerate(axes):
+        if i == 0:
+            img = query_image
+            # axis.text(74, 25, query, size=9)
+            axis.text(74, 45, 'Original image', size=9)
+        else:
+            img = top_images[i - 1][1]
+            axis.text(74, 45, str(top_images[i - 1][0]), size=9)
+        axis.imshow(img, cmap='gray')
+        axis.tick_params(left=False, right=False, labelleft=False, labelbottom=False, bottom=False)
+    fig.suptitle(str(t) + ' most similar images - ' +
+                 utilities.similarity_measures[utilities.feature_models.index(vector_file_tokens[1])], size=10)
+    
+    plt.show()
+
+    #now you will have t images and t labels
+    top_k_train = []
+    for i in top_images:
+        top_k_train.append(top_images[1])
+
+    labels = get_feedback()
+    while not (len(labels) == t):
+        labels = get_feedback()
+
+    clf,node = get_predictor_object(top_k_train , labels)
+    
+    result = []
+    for i in similarity_image_map:
+        pred = clf.predict(i[1],node)
+        print(pred)
+        if pred == 1:
+            result.append(i)
+    
+    print(len(result))
+    print("done")
+
+
+        
+if __name__ == "__main__":
+    start_task6()    
