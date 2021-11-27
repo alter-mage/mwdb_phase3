9e4bd41931df9b9700e7d8068d37f837f5ff2471
Siddhant Srivastava <sidsrivast42@gmail.com>
2021-11-26 20:22:04 -0700

    task4 update for relevance feedback

--- task4.py
+++ task4.py
@@ -32,8 +32,24 @@ def retrieve_images(layered_hash_buckets, query_hash_codes, pruned_hash, k):
     return retrieve_image_indexes
 
 
-def start_task4():
-    print('here')
+def build_index(l, k, left_matrix, index_images, query):
+    hash_family = {}
+    random_vectors = [np.random.randn(k, len(left_matrix[0])) for _ in range(l)]
+    for index, hashes in enumerate(random_vectors):
+        hash_family['l' + str(index + 1)] = hashes
+
+    layered_hash_bucket = build_hash_buckets(hash_family, index_images)
+
+    query_hash_codes = []
+    for layer in hash_family:
+        query_hash_layer_value = hash_family[layer].dot(np.array(query))
+        query_hash_value_code = ''.join(['1' if hash_value > 0 else '0' for hash_value in query_hash_layer_value])
+        query_hash_codes.append(query_hash_value_code)
+
+    return layered_hash_bucket, query_hash_codes
+
+
+def get_top_images(k=0):
     l = int(input('enter num of layers: '))
     k = int(input('enter num of hashes per layer: '))
     vector_file = input('enter vector file: ')
@@ -57,35 +73,24 @@ def start_task4():
         images.append(img)
         features.append(img_vector)
 
-    query_image_path = os.path.join(os.getcwd(), input('enter query image name')+'.png')
+    query_image_path = os.path.join(os.getcwd(), input('enter query image name') + '.png')
     query_image = cv2.imread(query_image_path, cv2.IMREAD_GRAYSCALE)
     query_image_vector = feature_model(query_image)
 
     features.append(query_image_vector)
-    # images = min_max_scaler.transform(images)
-    # transformed_images = query_transformation_model(images, right_matrix)
-
-    Scaler = MinMaxScaler()
-    min_max_images = Scaler.fit_transform(features)
-    transformed_images = query_transformation_model(min_max_images, right_matrix)
+    scaled_features = min_max_scaler.transform(features)
+    transformed_images = query_transformation_model(scaled_features, right_matrix)
 
     index_images = transformed_images[:-1]
     query = transformed_images[-1]
 
-    hash_family = {}
-    random_vectors = [np.random.randn(k, len(left_matrix[0])) for _ in range(l)]
-    for index, hashes in enumerate(random_vectors):
-        hash_family['l'+str(index+1)] = hashes
+    layered_hash_bucket, query_hash_codes = build_index(l, k, left_matrix, index_images, query)
 
-    layered_hash_bucket = build_hash_buckets(hash_family, index_images)
+    if not k:
+        t = int(input('enter number of retrievals: '))
+    else:
+        t = k
 
-    query_hash_codes = []
-    for layer in hash_family:
-        query_hash_layer_value = hash_family[layer].dot(np.array(query))
-        query_hash_value_code = ''.join(['1' if hash_value > 0 else '0' for hash_value in query_hash_layer_value])
-        query_hash_codes.append(query_hash_value_code)
-
-    t = int(input('enter number of retrievals: '))
     retrieved_image_indexes, pruned_hash = [], -1
     while len(set(retrieved_image_indexes)) < t:
         pruned_hash += 1
@@ -95,19 +100,24 @@ def start_task4():
     unique_images = [index_images[image_index] for image_index in unique_image_indexes]
     similarity_map = similarity_model(query, unique_images)
     similarity_image_map = [[score, images[index]] for score, index in zip(similarity_map, unique_image_indexes)]
-    similarity_image_map = sorted(similarity_image_map, key=lambda x: x[0], reverse=True)[:t]
+    similarity_image_map = sorted(similarity_image_map, key=lambda x: x[0], reverse=True)
 
+    return similarity_image_map, t, query_image, vector_file_tokens
+
+
+def start_task4():
+    similarity_image_map, t, query_image, vector_file_tokens = get_top_images()
+
+    top_images = similarity_image_map[:t]
     fig, axes = plt.subplots(t + 1, 1)
-    topk = []
     for i, axis in enumerate(axes):
         if i == 0:
             img = query_image
             # axis.text(74, 25, query, size=9)
             axis.text(74, 45, 'Original image', size=9)
         else:
-            img = similarity_image_map[i-1][1]
-            axis.text(74, 45, str(similarity_image_map[i-1][0]), size=9)
-            topk.append(img)
+            img = top_images[i - 1][1]
+            axis.text(74, 45, str(top_images[i - 1][0]), size=9)
         axis.imshow(img, cmap='gray')
         axis.tick_params(left=False, right=False, labelleft=False, labelbottom=False, bottom=False)
     fig.suptitle(str(t) + ' most similar images - ' +
